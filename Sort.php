45c483f41a6f9dec0c62901550c4b4ed68387ec6
Faijur199 <faijurahaman4@gmail.com>
2025-12-12 21:43:45 +0545

    Sort.php

--- Sort.php
+++ Sort.php
@@ -1,3 +1,16 @@
 <?php
-
+$arr = [5, 1, 9, 3, 7];
+$asc = $arr;
+sort($asc);
+echo "Ascending\n: ";
+foreach ($asc as $num) {
+    echo $num . " ";
+}
+echo "\n";
+$desc = $arr;
+rsort($desc);
+echo "Descending\n: ";
+foreach ($desc as $num) {
+    echo $num . " ";
+}
 ?>
